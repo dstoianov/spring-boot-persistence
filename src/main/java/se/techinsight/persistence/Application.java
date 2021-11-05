cbf8c2eb010634838327ede0d101fe9ad546bd27
Denys Stoianov <denys.stoianov@raisin.com>
2021-11-05 11:46:24 +0100

    add JpaAuditingConfiguration

--- src/main/java/se/techinsight/persistence/Application.java
+++ src/main/java/se/techinsight/persistence/Application.java
@@ -2,9 +2,6 @@ package se.techinsight.persistence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
 
 @SpringBootApplication
 public class Application {
